d222ba70513dde8194510415467224fe253f0375
hyemin916 <hmpk09@daum.net>
2021-07-22 15:10:23 +0900

    TS Quiz : Loading❗

--- typescript/types-projects/types-projects-quiz/loading.ts
+++ typescript/types-projects/types-projects-quiz/loading.ts
@@ -20,6 +20,28 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(loadState: ResourceLoadState) {
+    let text: string;
+    switch (loadState.state) {
+      case 'loading': {
+        text = '👀 loading...';
+        break;
+      }
+      case 'success': {
+        text = `😃 ${loadState.response.body}`;
+        break;
+      }
+      case 'fail': {
+        text = `😱 ${loadState.reason}`;
+        break;
+      }
+      default:
+        throw Error('unknown load state');
+    }
+
+    console.log(text);
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
