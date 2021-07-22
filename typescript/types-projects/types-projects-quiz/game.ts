7ce2edb602520f783275f62bbc120ecd78d735bd
hyemin916 <hmpk09@daum.net>
2021-07-22 14:46:04 +0900

    TS Quiz : Game❗

--- typescript/types-projects/types-projects-quiz/game.ts
+++ typescript/types-projects/types-projects-quiz/game.ts
@@ -1,6 +1,42 @@
 /**
  * Let's make a game 🕹
  */
+
+type Position = {
+  x: number;
+  y: number;
+};
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+let position: Position = {
+  x: 0,
+  y: 0,
+};
+
+function move(direction: Direction) {
+  switch (direction) {
+    case 'up': {
+      position.y += 1;
+      break;
+    }
+    case 'down': {
+      position.y -= 1;
+      break;
+    }
+    case 'left': {
+      position.x -= 1;
+      break;
+    }
+    case 'right': {
+      position.x += 1;
+      break;
+    }
+    default: {
+      throw Error('unknown direction');
+    }
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
