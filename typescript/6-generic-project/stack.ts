4a967654e82cfa441cfa052975f9bddc4683e2cd
hyemin916 <hmpk09@daum.net>
2021-07-26 16:26:30 +0900

    Note : Generic

--- typescript/6-generic-project/stack.ts
+++ typescript/6-generic-project/stack.ts
@@ -1,17 +1,17 @@
-interface Stack {
+interface Stack<T> {
   readonly size: number;
-  push(value: string): void;
-  pop(): void;
+  push(value: T): void;
+  pop(): T;
 }
 
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
+type StackNode<T> = {
+  readonly value: T;
+  readonly next?: StackNode<T>;
 };
 
-class StackImpl implements Stack {
+class StackImpl<T> implements Stack<T> {
   private _size: number = 0;
-  private head?: StackNode;
+  private head?: StackNode<T>;
 
   constructor(private capacity: number) {
     this.capacity = capacity;
@@ -19,16 +19,16 @@ class StackImpl implements Stack {
   get size() {
     return this._size;
   }
-  push(value: string) {
+  push(value: T) {
     if (this.size === this.capacity) {
       throw new Error('Stack is full!');
     }
-    const node: StackNode = { value, next: this.head };
+    const node = { value, next: this.head }; //타입 추론 활용
     this.head = node;
     this._size++;
   }
 
-  pop(): string {
+  pop(): T {
     if (this.head == null) {
       throw new Error('Stack is empty!');
     }
@@ -39,10 +39,18 @@ class StackImpl implements Stack {
   }
 }
 
-const stack = new StackImpl(5);
+const stack = new StackImpl<string>(5); //string 데이터 이용할 것이라고 정의
 stack.push('Ellie 1');
 stack.push('Bob 2');
 stack.push('Steve 3');
 while (stack.size !== 0) {
   console.log(stack.pop());
+}
+
+const stack2 = new StackImpl<number>(5); //string 데이터 이용할 것이라고 정의
+stack2.push(123);
+stack2.push(456);
+stack2.push(789);
+while (stack2.size !== 0) {
+  console.log(stack2.pop());
 }
