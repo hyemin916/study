b9e67d05ea9611cf6f845636809756713068f3d0
hyemin916 <hmpk09@daum.net>
2021-07-26 14:20:18 +0900

    TS Quiz : stack💡

--- typescript/4-oop-project/stack.ts
+++ typescript/4-oop-project/stack.ts
@@ -4,45 +4,47 @@ interface Stack {
   pop(): void;
 }
 
-class Item {
-  constructor(public value: string, public next: Item | null) {
-    this.value = value;
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
+
+class StackImpl implements Stack {
+  private _size: number = 0;
+  private head?: StackNode;
+
+  constructor(private capacity: number) {
+    this.capacity = capacity;
   }
-  getNext(): Item {
-    if (!this.next) {
-      throw Error("can't find next item");
-    }
-    return this.next;
+  get size() {
+    return this._size;
   }
-}
-
-class StackMaker implements Stack {
-  private head: Item | null = null;
-
-  constructor(readonly size: number) {}
-
   push(value: string) {
-    const item = new Item(value, this.head);
-    this.head = item;
+    if (this.size === this.capacity) {
+      throw new Error('Stack is full!');
+    }
+    const node: StackNode = { value, next: this.head };
+    this.head = node;
+    this._size++;
   }
 
-  pop() {
-    const top = this.head;
-    if (!top) {
-      throw Error('no item');
+  pop(): string {
+    if (this.head == null) {
+      throw new Error('Stack is empty!');
     }
-    this.head = top.getNext();
-    top.next = null;
-    let item: Item | null = this.head;
-    while (item) {
-      console.log(item.value);
-      item = item.next;
-    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
   }
 }
 
-const stack = new StackMaker(5);
-stack.push('hello');
-stack.push('world');
-stack.push('hi');
+const stack = new StackImpl(5);
+stack.push('Ellie 1');
+stack.push('Bob 2');
+stack.push('Steve 3');
+while (stack.size !== 0) {
+  console.log(stack.pop());
+}
+
 stack.pop();
