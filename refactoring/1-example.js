85126ced944421a4e84ab10708a2f9170d718a3c
hyemin <iammin916@gmail.com>
2021-08-25 21:58:47 +0900

    Move getting total functions

--- refactoring/1-example.js
+++ refactoring/1-example.js
@@ -17,12 +17,15 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
+
     return result;
   }
 
@@ -63,6 +66,26 @@ function statement(invoice, plays) {
     return result;
   }
 
+  function totalAmount(data) {
+    let result = 0;
+
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
+
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+
+    return result;
+  }
+
   return renderPlainText(statementData);
 }
 
@@ -86,24 +109,6 @@ function renderPlainText(data) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber);
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-
-    return result;
   }
 }
 
