e21b914ea2a3d7a40a45702913410105a7dbd0dd
hyemin <iammin916@gmail.com>
2021-08-25 22:32:24 +0900

    Move volumeCredits funtion into class

--- refactoring/1-example/createStatementData.js
+++ refactoring/1-example/createStatementData.js
@@ -27,6 +27,16 @@ class PerformanceCaculator {
 
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
+    return result;
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -46,22 +56,13 @@ export default function createStatementData(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
     result.amount = calculator.amout;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
 
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-
-    if ("comedy" === perf.play.type) result += Math.floor(perf.audience / 5);
-
-    return result;
   }
 
   function totalAmount(data) {
