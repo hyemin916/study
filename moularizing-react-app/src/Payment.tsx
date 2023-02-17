6731578c0b27302851ed04b6b4bf38df2391d777
박혜민 <hyemin@ktown4u.com>
2023-02-17 10:28:51 +0900

    move hook

--- moularizing-react-app/src/Payment.tsx
+++ moularizing-react-app/src/Payment.tsx
@@ -1,44 +1,5 @@
-import React, { useEffect, useState } from "react";
-import PaymentMethod from "./PaymentMethod";
-
-const payInCash = new PaymentMethod({ name: "cash" });
-
-const convertPaymentMethods = (methods: RemotePaymentMethod[]) => {
-  if (methods.length === 0) {
-    return [];
-  }
-
-  const extended: PaymentMethod[] = methods.map(
-    (method) => new PaymentMethod(method)
-  );
-  extended.push(payInCash);
-
-  return extended;
-};
-
-const usePaymentMethods = () => {
-  const [paymentMethods, setPaymentMethods] = useState<LocalPaymentMethod[]>(
-    []
-  );
-
-  useEffect(() => {
-    const fetchPaymentMethods = async () => {
-      const url = "https://online-ordering.com/api/payment-methods";
-
-      const response = await fetch(url);
-      const methods: RemotePaymentMethod[] = await response.json();
-
-      const extended = convertPaymentMethods(methods);
-      setPaymentMethods(extended);
-    };
-
-    fetchPaymentMethods();
-  }, []);
-
-  return {
-    paymentMethods,
-  };
-};
+import React from "react";
+import usePaymentMethods from "./usePaymentMethods";
 
 const Payment = ({ amount }: { amount: number }) => {
   const { paymentMethods } = usePaymentMethods();
---
+++ moularizing-react-app/src/usePaymentMethods.ts
@@ -0,0 +1,43 @@
+import PaymentMethod from "./PaymentMethod";
+import { useEffect, useState } from "react";
+
+const payInCash = new PaymentMethod({ name: "cash" });
+
+const convertPaymentMethods = (methods: RemotePaymentMethod[]) => {
+  if (methods.length === 0) {
+    return [];
+  }
+
+  const extended: PaymentMethod[] = methods.map(
+    (method) => new PaymentMethod(method)
+  );
+  extended.push(payInCash);
+
+  return extended;
+};
+
+const usePaymentMethods = () => {
+  const [paymentMethods, setPaymentMethods] = useState<LocalPaymentMethod[]>(
+    []
+  );
+
+  useEffect(() => {
+    const fetchPaymentMethods = async () => {
+      const url = "https://online-ordering.com/api/payment-methods";
+
+      const response = await fetch(url);
+      const methods: RemotePaymentMethod[] = await response.json();
+
+      const extended = convertPaymentMethods(methods);
+      setPaymentMethods(extended);
+    };
+
+    fetchPaymentMethods();
+  }, []);
+
+  return {
+    paymentMethods,
+  };
+};
+
+export default usePaymentMethods;
