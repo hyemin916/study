ce45a25c850ed2dd4f22e433d7acf95542cea351
박혜민 <hyemin@ktown4u.com>
2023-02-19 16:41:23 +0900

    add annotation

---
+++ mvc-practice/src/main/java/org/example/annotation/RequestMapping.java
@@ -0,0 +1,11 @@
+package org.example.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface RequestMapping {
+    String value() default "";
+
+    RequestMethod[] method() default {};
+}
