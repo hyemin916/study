bdcb86a37b26ebed01ebac52485c7a963e161e87
박혜민 <hyemin@ktown4u.com>
2023-02-19 20:19:35 +0900

    Add controller

--- mvc-practice/src/main/java/org/example/controller/HomeController.java
+++ mvc-practice/src/main/java/org/example/controller/HomeController.java
@@ -1,17 +1,15 @@
 package org.example.controller;
 
-import org.example.annotation.Controller;
 import org.example.annotation.RequestMapping;
 import org.example.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Controller
-public class HomeController {
+public class HomeController  implements Controller {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home(HttpServletRequest request, HttpServletResponse response) {
-        return "home";
+    @Override
+    public String handleRequest(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        return "home.jsp";
     }
 }
