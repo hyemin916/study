3b5fd608040fd9f8c59ec5b9f4eab3856bbede97
박혜민 <hyemin@ktown4u.com>
2023-02-19 18:05:53 +0900

    refactor test

--- mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -7,20 +7,26 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.lang.ref.Reference;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(Reference.class);
     @Test
     void controllerScan() {
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+
+        logger.debug("benas: [{}]", beans);
+    }
+
+    private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
         Reflections reflections = new Reflections("org.example");
 
         Set<Class<?>> beans = new HashSet<>();
-        beans.addAll(reflections.getTypesAnnotatedWith(Controller.class));
-        beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
-
-        logger.debug("benas: [{}]", beans);
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+        return beans;
     }
 }
