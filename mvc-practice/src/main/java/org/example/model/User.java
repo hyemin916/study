1da4545a1f08f0b69a91626ed3ce27f12f4a911e
박혜민 <hyemin@ktown4u.com>
2023-02-19 18:09:01 +0900

    add User

---
+++ mvc-practice/src/main/java/org/example/model/User.java
@@ -0,0 +1,29 @@
+package org.example.model;
+
+import java.util.Objects;
+
+public class User {
+    private String id;
+    private String name;
+
+    public User(final String id, final String name) {a
+        this.id = id;
+        this.name = name;
+    }
+
+    public boolean equalsUser(User user) {
+        return this.equals(user);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof final User user)) return false;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
