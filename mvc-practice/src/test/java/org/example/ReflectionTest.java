d3b97238cfc7310d9d9e44d78ce8334848255be1
박혜민 <hyemin@ktown4u.com>
2023-02-19 18:15:26 +0900

    test load class

--- mvc-practice/src/test/java/org/example/ReflectionTest.java
+++ mvc-practice/src/test/java/org/example/ReflectionTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
 import org.example.model.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -33,6 +34,24 @@ public class ReflectionTest {
         logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
         logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
         logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @DisplayName("힙 영역에 로드된 클래스를 가져오는 방법")
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1
+        Class<User> clazz = User.class;
+
+        // 2
+        User user = new User("1", "name");
+        final Class<? extends User> aClass = user.getClass();
+
+        // 3
+        final Class<?> aClass2 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz: [{}]", clazz);
+        logger.debug("aClass: [{}]", aClass);
+        logger.debug("aClass2: [{}]", aClass2);
     }
 
     private Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
