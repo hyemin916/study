490c6e4c8bc5c49919327647757b5cdb8e02da21
박혜민 <hyemin@ktown4u.com>
2023-02-19 20:38:23 +0900

    .

--- mvc-practice/src/main/java/org/example/controller/UserListController.java
+++ mvc-practice/src/main/java/org/example/controller/UserListController.java
@@ -2,11 +2,13 @@ package org.example.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class UserListController implements Controller{
 
     @Override
     public String handleRequest(final HttpServletRequest req, final HttpServletResponse res) throws Exception {
+        req.setAttribute("users", List.of());
         return "/user/list.jsp";
     }
 }
