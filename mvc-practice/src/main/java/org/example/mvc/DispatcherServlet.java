1148629e2ffd48e1eb5891098f076f80e833efae
박혜민 <hyemin@ktown4u.com>
2023-02-20 20:48:27 +0900

    implement viewResolver

--- mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
+++ mvc-practice/src/main/java/org/example/mvc/DispatcherServlet.java
@@ -2,6 +2,9 @@ package org.example.mvc;
 
 import org.example.mvc.annotation.RequestMethod;
 import org.example.mvc.controller.Controller;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,15 +14,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
     private RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
+    private List<ViewResolver> viewResolvers;
 
     @Override
     public void init() throws ServletException {
         rmhm.init();
+
+        viewResolvers = Collections.singletonList(new JspViewResolver());
     }
 
     @Override
@@ -29,6 +39,12 @@ public class DispatcherServlet extends HttpServlet {
             final Controller handler = rmhm.findHandler(new HandlerKey(RequestMethod.valueOf(req.getMethod()), req.getRequestURI()));
             // redirect & forward
             final String viewName = handler.handleRequest(req, resp);
+
+            for (ViewResolver viewResolver : viewResolvers) {
+                final View view = viewResolver.resolveViews(viewName);
+                view.render(new HashMap<>, req, resp);
+            }
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
