ba5f9c8031a1fb541dc704057a71da9514aa3a06
박혜민 <hyemin@ktown4u.com>
2023-02-20 15:11:35 +0900

    add agreeToDonate

--- moularizing-react-app/src/components/Payment.tsx
+++ moularizing-react-app/src/components/Payment.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import usePaymentMethods from "../hooks/usePaymentMethods";
 import { PaymentMethods } from "./PaymentMethods";
 
 const Payment = ({ amount }: { amount: number }) => {
   const { paymentMethods } = usePaymentMethods();
+  const [agreeToDonate, setAgreeToDonate] = useState<boolean>(false);
+
+  const { total, tip } = {
+    total: agreeToDonate ? Math.floor(amount + 1) : amount,
+    tip: parseFloat((Math.floor(amount + 1) - amount).toPrecision(10)),
+  };
 
   return (
     <div>
