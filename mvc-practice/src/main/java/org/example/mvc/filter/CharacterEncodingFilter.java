aa10564fc7004fe1476cf69ecfde0dbb72fb1f80
박혜민 <hyemin@ktown4u.com>
2023-02-20 20:59:33 +0900

    add encoding

---
+++ mvc-practice/src/main/java/org/example/mvc/filter/CharacterEncodingFilter.java
@@ -0,0 +1,28 @@
+package org.example.mvc.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
